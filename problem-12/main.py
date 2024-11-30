5670df27a003b74dee22ddc68d96b5608b036cf5
umrzoqovndev1998 <umrzoqovndev1998@gmail.com>
2024-11-30 13:24:54 +0500

    problem-12

--- problem-12/main.py
+++ problem-12/main.py
@@ -1,53 +1,51 @@
 
 def intToRoman(num: int) -> str:
         int_to_roman = ''
-        k = num//1000
-        for i in range(k):
+        number_M = num//1000
+        for i in range(number_M):
             int_to_roman+='M'
-        m = num - k*1000
-        d = m//900
-        for i in range(d):
+        num = num - number_M*1000
+        number_CM = num//900
+        for i in range(number_CM):
             int_to_roman+='CM'
-        c = m - d*900
-        s = c//500
-        for i in range(s):
+        num = num - number_CM*900
+        number_D = num//500
+        for i in range(number_D):
             int_to_roman+='D'
-        t = c - s*500
-        y = t//100
-        if(y==4):
+        num = num - number_D*500
+        number_C = num//100
+        if(number_C==4):
             int_to_roman+='CD'
         else:
-            for i in range(y):
+            for i in range(number_C):
                 int_to_roman+='C'
-        z = t - y*100
-        w = z//90
-        for i in range(w):
+        num = num - number_C*100
+        number_XC = num//90
+        for i in range(number_XC):
             int_to_roman+='XC'
-        h = z - w*90
-        g = h//50
-        for i in range(g):
+        num = num - number_XC*90
+        number_L = num//50
+        for i in range(number_L):
             int_to_roman+='L'
-        y1 = h - g *50
-        z1 = y1//10
-        if(z1==4):
+        num = num - number_L *50
+        number_X = num//10
+        if(number_X==4):
             int_to_roman+='XL'
         else:
-            for i in range(z1):
+            for i in range(number_X):
                 int_to_roman+='X'
-        t1 = y1 - z1*10
-        if(t1==9):
+        num = num - number_X*10
+        if(num==9):
             int_to_roman+='IX'
         else:
-            k1=t1//5
-            for i in range(k1):
+            number_V = num//5
+            for i in range(number_V):
                 int_to_roman+='V'
-            w1 = t1 - k1*5
-            if(w1==4):
+            number_I = num - number_V*5
+            if(number_I==4):
                 int_to_roman+='IV'
             else:
-                for i in range(w1):
+                for i in range(number_I):
                     int_to_roman+='I'
 
         return int_to_roman
-
-print(intToRoman(876))
